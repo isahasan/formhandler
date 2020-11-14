5ff1859c95318097f79cc52aeac2fcb5a580fbd9
isahasan <esahasan42@gmail.com>
2020-11-14 22:44:42 +0600

    Updates

---
+++ precache-manifest.4acda84a6c5f39643ec45746014098aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac9258fde710213671b79257103a845",
+    "url": "/formhandler/index.html"
+  },
+  {
+    "revision": "4410a3875dc5291c6d4c",
+    "url": "/formhandler/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "fe96e71c62a26aa57375",
+    "url": "/formhandler/static/js/2.6fdc75f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/formhandler/static/js/2.6fdc75f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4410a3875dc5291c6d4c",
+    "url": "/formhandler/static/js/main.ab7004c0.chunk.js"
+  },
+  {
+    "revision": "c507a50471e3b0355ff0",
+    "url": "/formhandler/static/js/runtime-main.251f6de6.js"
+  }
+]);
